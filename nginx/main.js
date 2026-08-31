789f19e9e801e11ac3963e6fc1b02cf427d10472
Daniel Micay <daniel.micay@grapheneos.org>
2026-08-30 20:24:17 -0400

    nginx: add normalization for gmail.com addresses
    This is needed to match the normalization done by StopForumSpam for
    gmail.com addresses.

--- nginx/main.js
+++ nginx/main.js
@@ -6,7 +6,19 @@ async function extractEmail(r) {
             r.return(400);
             return;
         }
-        r.variables.extracted_email = data.email;
+        const parts = data.email.split("@");
+        if (parts.length != 2) {
+            r.error("invalid email field");
+            r.return(400);
+            return;
+        }
+        const domain = parts[1];
+        if (domain == "gmail.com") {
+            const username = parts[0].split('+')[0].replaceAll(".", "");
+            r.variables.extracted_email = username + "@" + domain;
+        } else {
+            r.variables.extracted_email = data.email;
+        }
     } catch (e) {
         r.error(`JSON parsing failed: ${e.message}`);
         r.return(400);
