c3f4519565116b6f70fcfffa88285e03e0d7cf28
Daniel Micay <daniel.micay@grapheneos.org>
2026-08-31 14:15:18 -0400

    njs: reduce scope of try-catch

--- nginx/main.js
+++ nginx/main.js
@@ -1,28 +1,29 @@
 async function extractEmail(r) {
+    let data;
     try {
-        const data = await r.readRequestJSON();
-        if (!data.email) {
-            r.error("missing email field in /register request");
-            r.return(400);
-            return;
-        }
-        const parts = data.email.split("@");
-        if (parts.length != 2) {
-            r.error("invalid email field");
-            r.return(400);
-            return;
-        }
-        const domain = parts[1];
-        if (domain == "gmail.com") {
-            const username = parts[0].split('+')[0].replaceAll(".", "");
-            r.variables.extracted_email = username + "@" + domain;
-        } else {
-            r.variables.extracted_email = data.email;
-        }
+        data = await r.readRequestJSON();
     } catch (e) {
         r.error(`JSON parsing failed: ${e.message}`);
         r.return(400);
         return;
+    }
+    if (!data.email) {
+        r.error("missing email field in /register request");
+        r.return(400);
+        return;
+    }
+    const parts = data.email.split("@");
+    if (parts.length != 2) {
+        r.error("invalid email field");
+        r.return(400);
+        return;
+    }
+    const domain = parts[1];
+    if (domain == "gmail.com") {
+        const username = parts[0].split('+')[0].replaceAll(".", "");
+        r.variables.extracted_email = username + "@" + domain;
+    } else {
+        r.variables.extracted_email = data.email;
     }
     r.internalRedirect("/register-internal");
 }
