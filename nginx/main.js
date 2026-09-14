adbfba222b8c59d82d8cde05791a28015fac3d84
Daniel Micay <daniel.micay@grapheneos.org>
2026-09-14 11:40:51 -0400

    normalize googlemail.com to gmail.com for registration blocking

--- nginx/main.js
+++ nginx/main.js
@@ -19,9 +19,9 @@ async function extractEmail(r) {
         return;
     }
     const domain = parts[1];
-    if (domain === "gmail.com") {
+    if (domain === "gmail.com" || domain === "googlemail.com") {
         const username = parts[0].split('+')[0].replaceAll(".", "");
-        r.variables.extracted_email = username + "@" + domain;
+        r.variables.extracted_email = username + "@gmail.com";
     } else {
         r.variables.extracted_email = data.email;
     }
